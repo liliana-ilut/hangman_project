8fdadca772d98662e2b03f7c6436195008b1a1c8
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2023-01-24 14:50:55 -0600

    added TODO1 from challenge 2

--- hangman.py
+++ hangman.py
@@ -6,16 +6,30 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 # print the chosen_word to see if it works corectly :
-print(chosen_word)
+# print(chosen_word)
+print(f"The chosen word is {chosen_word}.")
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# the code was moved to challenge 2, todo 1
+# guess = input("Guess a letter:\n").lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# the code was moved to challenge 2, todo 2
+
+# for letter in chosen_word:
+#   if letter == guess:
+#     print("right")
+#   else:
+#     print("wrong")
+
+#Challenge 2
+# #TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.    
 
 guess = input("Guess a letter:\n").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
-for letter in chosen_word:
-  if letter == guess:
-    print("right")
-  else:
-    print("wrong")
+display = []
+for _ in chosen_word:
+    display += ["_"]
+print(display)    
