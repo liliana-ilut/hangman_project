209cf2d9dfbc50a53c8e26ed988d04184c789db2
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2023-01-24 13:54:09 -0600

    added TODO1 from challenge1

--- hangman.py
+++ hangman.py
@@ -10,4 +10,6 @@ print(chosen_word)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
+guess = input("Guess a letter:\n").lower()
+
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
